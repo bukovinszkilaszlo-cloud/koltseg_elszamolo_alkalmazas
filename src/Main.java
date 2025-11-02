bf7b8486b2ec7e104e722b4923ca18e85b643757
lilman188 <reskoimre@gmail.com>
2025-11-02 16:53:33 +0100

    Modern layout

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            // Modern layout
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         SwingUtilities.invokeLater(ExpenseFrame::new);
     }
 }
